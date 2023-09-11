e8197b680c5439ebe9e9753e1591d46c66102519
dylan_huang <wongwc1998@gmail.com>
2023-09-11 20:33:12 +0800

    add test api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { BookDialog } from "@/components/bookDialog";
-import BookCardsArea from "@/components/BookCardsArea";
+import BookList from "@/components/BookList";
+import { getBooks } from "@/components/testAPI";
 
-export default function Home() {
+export default async function Home() {
+  const books = await getBooks();
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-start p-24 gap-4">
       <h1>This is Library</h1>
       <BookDialog />
       <BookList books={books} />
--- src/components/submitStuff.tsx
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../app/db";
-import BookData from "@/types/Book";
-
-export default async function submitStuff(data: BookData) {
-
-    await prisma.book.create({data: data});
-    console.log(data);
-  }
