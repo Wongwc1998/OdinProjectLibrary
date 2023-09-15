f6063dccef1c0d6deb8bf6bcb5c18a78d605896b
dylan_huang <wongwc1998@gmail.com>
2023-09-15 11:37:30 +0800

    separate prisma crud

---
+++ src/app/api/PrismaCRUD.tsx
@@ -0,0 +1,23 @@
+"use server";
+
+import { prisma } from "../db";
+import BookData from "@/types/Book";
+
+const addBookPrisma = async (data: BookData): Promise<void> => {
+  await prisma.book.create({ data: data });
+  console.log(data);
+};
+
+const getBooksPrisma = async (): Promise<BookData[]> => {
+  return await prisma.book.findMany();
+};
+
+const deleteBookPrisma = async (id: string): Promise<void> => {
+  await prisma.book.delete({ where: { id: id } });
+};
+
+const updateBookPrisma = async (id: string, data: BookData): Promise<void> => {
+  await prisma.book.update({ where: { id: id }, data: data });
+};
+
+export { addBookPrisma, getBooksPrisma, deleteBookPrisma, updateBookPrisma };
